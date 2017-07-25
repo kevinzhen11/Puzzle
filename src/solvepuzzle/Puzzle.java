fbb7a8e32e74bcf226f0e52091a0a58cea74d009
kevinzhen11 <kzhen@albany.edu>
2017-07-24 21:59:23 -0400

    Implemented Letter object into file

--- src/solvepuzzle/Puzzle.java
+++ src/solvepuzzle/Puzzle.java
@@ -5,19 +5,51 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /*
- *  NOTE: The file_in file has to be adjacent to the src folder in this Eclipse project, as in not in src/solvepuzzle or inside
- *  src, as Puzzle.java will throw a FileNotFoundException
+ *  NOTE: The file_in file has to be adjacent to the src folder in this Eclipse project, eg. not in src/solvepuzzle or inside
+ *  src, as Puzzle.java will throw a FileNotFoundException if that happens
  */
 
 public class Puzzle {
 	
-	public static String file_in = "puzzle.csv"; 
-	public static Scanner my_scanner = new Scanner(System.in);
+	private static String file_in = "puzzle.csv"; 
+	private static Scanner my_scanner = new Scanner(System.in);
 	private static Scanner file_scanner;
-	static int SIZE = 20;
+	private static int SIZE = 20;
 	
-	public static String[][] readFile(String file_in) throws FileNotFoundException{
-		String[][] table = new String[SIZE][SIZE]; // 2D array to hold the table in .csv file
+	public static Letter left(Letter[][] table, Letter current){
+		return table[current.getX()][current.getY() - 1];
+	}
+	
+	public static Letter right(Letter[][] table, Letter current){
+		return table[current.getX()][current.getY() + 1];
+	}
+	
+	public static Letter up(Letter[][] table, Letter current){
+		return table[current.getX() - 1][current.getY()];
+	}
+	
+	public static Letter down(Letter[][] table, Letter current){
+		return table[current.getX() + 1][current.getY()];
+	}
+	
+	public static Letter topLeft(Letter[][] table, Letter current){
+		return table[current.getX() - 1][current.getY() - 1];
+	}
+	
+	public static Letter topRight(Letter[][] table, Letter current){
+		return table[current.getX() - 1][current.getY() + 1];
+	}
+	
+	public static Letter bottomLeft(Letter[][] table, Letter current){
+		return table[current.getX() + 1][current.getY() - 1];
+	}
+	
+	public static Letter bottomRight(Letter[][] table, Letter current){
+		return table[current.getX() + 1][current.getY() + 1];
+	}
+	
+	public static Letter[][] readFile(String file_in) throws FileNotFoundException{
+		Letter[][] table = new Letter[SIZE][SIZE]; // 2D array to hold the table in .csv file
 		String[] line_split = new String[SIZE]; // String to hold current line once it's split via commas
 		String curr_line; // String to hold current line
 		int counter = 0;
@@ -27,19 +59,40 @@ public class Puzzle {
 			curr_line = file_scanner.next();
 			line_split = curr_line.split(",");
 			for(int i = 0; i < SIZE; i++){
-				table[counter][i] = line_split[i];
+				table[counter][i] = new Letter(counter,i,line_split[i]);
 			}
 			counter += 1;
 		}
 		return table;
 	}
 	
+	public void searchWord(Letter[][] table, String word){
+		String[] letters = word.split(""); // split the word into an arr of letter
+		return;
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
-		System.out.print("Enter the word you wish to find in " + file_in + ": ");
-		String word = my_scanner.next(); // take in user input
+		// System.out.print("Enter the word you wish to find in " + file_in + ": ");
+		// String word = my_scanner.next(); // take in user input
 
-		String[][] table = readFile(file_in); // get table from file
+		Letter[][] table = readFile(file_in); // get table from file
 		
+		for(int i = 0 ; i < SIZE; i++){
+			for(int j = 0 ; j < SIZE; j++){
+				System.out.print(table[i][j].toStringWords() + "  ");
+			}
+			System.out.println();
+		}
+		
+		Letter test = table[1][5];
+		System.out.println(test.toString() + "\nLeft " + left(table, test).toString()
+				+ "\nRight " + right(table, test).toString()
+				+ "\nUp " + up(table, test).toString()
+				+ "\nDown " + down(table, test).toString()
+				+ "\nTop Left " + topLeft(table, test).toString()
+				+ "\nTop Right " + topRight(table, test).toString()
+				+ "\nBottom Left " + bottomLeft(table, test).toString()
+				+ "\nBottom Right " + bottomRight(table, test).toString());
 	}
 
 }
